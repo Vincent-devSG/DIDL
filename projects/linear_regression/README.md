2b9ba6971fb1bb677a782db38b50ebb765af6dd6
Vincent <vincent.pompei@outlook.fr>
2024-05-17 18:52:49 +0200

    feat: reformat project template

---
+++ projects/linear_regression/README.md
@@ -0,0 +1,24 @@
+Using the following kagle dataset: https://www.kaggle.com/datasets/mohansacharya/graduate-admissions/data
+
+The first implementation of linear regression will be done just using pandas and numpy.
+A second part will be done using pytorch.
+
+- Part 1:  
+I want to implement the linear regression model from scratch, using only numpy and pandas.  
+I made used jupyter notebook to explore the data. 
+
+List of things I done:
+- [x] Load the data.
+- [x] Explore the data.
+- [x] Clean the data. (No work needed)
+- [x] Visualize the data.
+- [x] GetData module to get the training and testing splits.
+
+List of ameliorations to do:
+- [ ] Evaluate the model.
+- [ ] Pipeline.
+- [ ] Add more visualizations.
+- [ ] Add Generalization, Weight Decay, K-Fold Cross Validation.
+
+- Part 2:
+I want to implement the linear regression model from scratch, using PyTorch.
--- projects/linear_regression/part1/training.py
+++ projects/linear_regression/part1/training.py
@@ -4,6 +4,8 @@ import getData
 import model
 import matplotlib.pyplot as plt
 import timeit
+import seaborn as sns
+
 
 # start the timer
 start = timeit.default_timer()
@@ -30,11 +32,13 @@ error_matrix = np.array([])
 
 w_hat, error_matrix = model.BatchGradientDescent(x_train, y_train, error_threshold, max_epochs, learning_rate, w, weight_decay, error_matrix)
 
-# plot target against predicted
-y_hat = np.dot(x_test, w_hat[1:]) + w_hat[0]
+# stop the timer
+stop = timeit.default_timer()
+print('Time: ', stop - start)
 
-# check error
-print(f'MSE: {model.MSE(y_test, y_hat)}')
+
+# plot target against predicted
+y_hat = np.dot(x_test, w[1:]) + w[0]
 
 # Negative values are not possible for the target also value should be between 0 and 1 
 # so we clip the values
@@ -44,16 +48,53 @@ y_test_sorted = np.sort(y_test)
 idx = np.argsort(y_test)
 y_hat_hat = y_hat[idx]
 
-# plot the target in red
-plt.plot(y_test_sorted, 'ro')
-# plot the predicted in blue
-plt.plot(y_hat_hat, 'bo')
+# calculate the R-squared
+r_squared = 1 - np.sum((y_test - y_hat) ** 2) / np.sum((y_test - np.mean(y_test)) ** 2)
+print(f'R-squared: {r_squared}')
 
-# stop the timer
-stop = timeit.default_timer()
-print('Time: ', stop - start)
+# plot the target in red
+plt.plot(y_test_sorted, 'r.')
+# plot the predicted in blue
+plt.plot(y_hat_hat, 'b.')
+
+# plot a line between the points
+for i in range(len(y_test)):
+    plt.plot([i, i], [y_test_sorted[i], y_hat_hat[i]], 'b-')
 
 plt.show()
 
+# Calculate the variance and the standard deviation of the error
+variance = np.var(y_test - y_hat)
+std_dev = np.sqrt(variance)
+
+print(f'Variance: {variance} Standard Deviation: {std_dev}')
+
+# make a ribbon plot of expected values
+plt.plot(y_test_sorted, 'r')
+plt.fill_between(np.arange(len(y_test)), y_test_sorted - std_dev, y_test_sorted + std_dev, color='red', alpha=0.3)
+plt.fill_between(np.arange(len(y_test)), y_test_sorted + std_dev, y_hat_hat , where = y_hat_hat > (y_test_sorted + std_dev), color='blue', alpha=0.3)
+plt.fill_between(np.arange(len(y_test)), y_test_sorted - std_dev, y_hat_hat , where = y_hat_hat < (y_test_sorted - std_dev), color='blue', alpha=0.3)
+plt.plot(y_hat_hat, 'b', linewidth = '0.5')
+plt.show()
 
 
+feature_weights = np.abs(w[1:])
+# Get feature names
+feature_names = train.columns[:-1]  # Assuming the last column is the target variable
+
+
+# Plotting
+plt.figure(figsize=(10, 6))
+sns.barplot(x=feature_weights, y=feature_names)
+plt.xlabel('Coefficient Magnitude')
+plt.ylabel('Features')
+plt.title('Magnitude of Coefficients')
+plt.show()
+
+# plot the error distribution
+error = y_test_sorted - y_hat_hat
+sns.histplot(error, kde=True)
+plt.xlabel('Error')
+plt.ylabel('Density')
+plt.title('Error Distribution')
+plt.show()
